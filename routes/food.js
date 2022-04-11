3cf40c925b65164065b91910297f9e0a517b1940
Lau Yu Yao <lauyuyao@Laus-MacBook-Pro.local>
2022-04-11 16:45:18 +0800

    updated the end point for the backend for add reviews
    added /stall to the food.js routes to differentiate the logout and adding of review

--- routes/food.js
+++ routes/food.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const food = require('../controllers/food');
-const jwtAuth = require('../middleware/jwtAuth');
-const multer = require('multer');
-const { storage } = require('../cloudinaryConfig');
+const food = require("../controllers/food");
+const jwtAuth = require("../middleware/jwtAuth");
+const multer = require("multer");
+const { storage } = require("../cloudinaryConfig");
 const upload = multer({ storage });
 
-const catchAsync = require('../middleware/errorHandler');
-router.get('/home', catchAsync(food.recommended));
-router.post('/new', [ jwtAuth, upload.single('file') ], catchAsync(food.new));
-router.get('/seed/seedHawkers', catchAsync(food.seedHawkers));
-router.get('/:id', jwtAuth, catchAsync(food.getOneStall));
+const catchAsync = require("../middleware/errorHandler");
+router.get("/home", catchAsync(food.recommended));
+router.post("/new", [jwtAuth, upload.single("file")], catchAsync(food.new));
+router.get("/seed/seedHawkers", catchAsync(food.seedHawkers));
+router.get("/stall/:id", jwtAuth, catchAsync(food.getOneStall));
 module.exports = router;
