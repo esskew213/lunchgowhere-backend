67e03878c5cc6b4c9558d15921ad63b57ab674e5
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-13 10:38:19 +0800

    added netlify to cors

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const connectDB = require('./db/db');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const express = require("express");
+const session = require("express-session");
+const connectDB = require("./db/db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-app.use(cors({ origin: [ 'http://localhost:3000', 'http://localhost:5001' ], credentials: true }));
+app.use(
+	cors({
+		origin: [
+			"http://localhost:3000",
+			"http://localhost:5001",
+			"https://sei-35-lunchgowhere.herokuapp.com/",
+			"https://lunchgowhere.netlify.app/"
+		],
+		credentials: true
+	})
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 ////
 const store = new MongoDBStore({
 	uri: process.env.MONGODB_URI,
-	collection: 'sessions'
+	collection: "sessions"
 });
 ////
 app.use(
@@ -28,17 +38,17 @@ app.use(
 		store: store
 	})
 );
-const userRoutes = require('./routes/user');
-app.use('/', userRoutes);
-const reviewRoutes = require('./routes/reviews');
-app.use('/review', reviewRoutes);
-const foodRoutes = require('./routes/food');
-app.use('/food', foodRoutes);
-const UserError = require('./UserError');
+const userRoutes = require("./routes/user");
+app.use("/", userRoutes);
+const reviewRoutes = require("./routes/reviews");
+app.use("/review", reviewRoutes);
+const foodRoutes = require("./routes/food");
+app.use("/food", foodRoutes);
+const UserError = require("./UserError");
 
 //// THROW ERROR IF USER TRIES TO ACCESS UNDEFINED ROUTES
-app.all('*', (req, res, next) => {
-	next(new UserError('Unknown endpoint', 404));
+app.all("*", (req, res, next) => {
+	next(new UserError("Unknown endpoint", 404));
 });
 
 // ERROR HANDLING MIDDLEWARE
@@ -51,7 +61,7 @@ app.use((err, req, res, next) => {
 	}
 });
 
-connectDB(process.env.MONGODB_URI);
+connectDB(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lunchgowhere");
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
 	console.log(`LISTENING ON PORT ${PORT}`);
